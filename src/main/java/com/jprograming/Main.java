227be5c728146d9a756ca64a54b8f510f04199b9
ramses2099 <ramses2099@gmail.com>
2024-01-17 10:18:53 -0400

    first commit

--- src/main/java/com/jprograming/Main.java
+++ src/main/java/com/jprograming/Main.java
@@ -1,12 +1,17 @@
 package com.jprograming;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("ok");
-
-
-
-
+        logger.info( "Hello World!" );
+        logger.debug( "Hello World!" );
+        logger.warn( "Hello World!" );
+        System.out.println("Test the System");
+        logger.error( "Hello World!" );
+        logger.fatal( "Hello World!" );
     }
 }
